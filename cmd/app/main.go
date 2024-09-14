e01aa9caa1fd369e1a24babeac2478b3bbb75a0a
Kazal Oshodi <kazoshodi@gmail.com>
2024-09-14 01:57:29 +0100

    tidy up code

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,11 +1,20 @@
 package main
 
 import (
+	"log"
 	server "music-exercise-tracking/internal/routes"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
-
+	if _, err := os.Stat("/.dockerenv"); os.IsNotExist(err) {
+		err := godotenv.Load()
+		if err != nil {
+			log.Fatalf("Error loading .env file")
+		}
+	}
 	server.Start()
 
 }
