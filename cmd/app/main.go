2fee368078de6a416284f88556c7640ddfc38efc
Kazal Oshodi <kazoshodi@gmail.com>
2024-09-15 23:06:05 +0100

    guess

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -3,17 +3,14 @@ package main
 import (
 	"log"
 	server "music-exercise-tracking/internal/routes"
-	"os"
 
 	"github.com/joho/godotenv"
 )
 
 func init() {
-	if _, err := os.Stat("/.dockerenv"); os.IsNotExist(err) {
-		err := godotenv.Load()
-		if err != nil {
-			log.Fatalf("Error loading .env file")
-		}
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file")
 	}
 }
 
