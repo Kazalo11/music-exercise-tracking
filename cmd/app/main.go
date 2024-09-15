4999b29a624be2fe20311e008ae6d3ea9837fecd
Kazal Oshodi <kazoshodi@gmail.com>
2024-09-15 23:59:26 +0100

    try this now

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,23 +1,19 @@
 package main
 
 import (
-	"log"
 	server "music-exercise-tracking/internal/routes"
-	"os"
-	"path/filepath"
 
 	"github.com/joho/godotenv"
 )
 
 func init() {
-	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
-	if err != nil {
-		log.Fatal(err)
-	}
-	environmentPath := filepath.Join(dir, ".env")
-	err = godotenv.Load(environmentPath)
-	if err != nil {
-		log.Fatal(err)
+	path := ".env"
+	for {
+		err := godotenv.Load(path)
+		if err == nil {
+			break
+		}
+		path = "../" + path
 	}
 }
 
