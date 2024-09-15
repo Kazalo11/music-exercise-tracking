1c057956ae08ed3264cad1450e76e7d7a532a014
Kazal Oshodi <kazoshodi@gmail.com>
2024-09-15 23:54:34 +0100

    trying this

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,15 @@
 #build stage
-FROM golang:1.23.1-bullseye AS build
+FROM golang:1.23.1-alpine AS build
 WORKDIR /app
 COPY go.mod go.sum ./
 RUN go mod download
 COPY . .
-RUN  go build -o server ./cmd/app
+RUN CGO_ENABLED=0 GOOS=linux go build -o server ./cmd/app
 
 #run stage
 FROM alpine:latest
-COPY --from=build /app/server /server
+WORKDIR /app
+COPY --from=build /app/server .
 EXPOSE 8080
-CMD ["/server"]
+CMD ["/app/server"]
 
--- cmd/app/main.go
+++ cmd/app/main.go
@@ -3,14 +3,21 @@ package main
 import (
 	"log"
 	server "music-exercise-tracking/internal/routes"
+	"os"
+	"path/filepath"
 
 	"github.com/joho/godotenv"
 )
 
 func init() {
-	err := godotenv.Load()
+	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
 	if err != nil {
-		log.Fatalf("Error loading .env file")
+		log.Fatal(err)
+	}
+	environmentPath := filepath.Join(dir, ".env")
+	err = godotenv.Load(environmentPath)
+	if err != nil {
+		log.Fatal(err)
 	}
 }
 
