08341f44045bd2dd02cbdd16cf023e56c89bd189
Kazal Oshodi <kazoshodi@gmail.com>
2024-09-16 00:24:45 +0100

    fix frontend build

---
+++ web/src/components/UserInput.tsx
@@ -0,0 +1,51 @@
+import { LockIcon, UnlockIcon } from "@chakra-ui/icons";
+import {
+  IconButton,
+  Input,
+  InputGroup,
+  InputRightElement,
+  VStack,
+} from "@chakra-ui/react";
+import React, { useState } from "react";
+
+interface UserInputProps {
+  onChange: (username: string, isSubmit: boolean) => void;
+}
+
+export function UserInput({ onChange }: UserInputProps) {
+  const [username, setUsername] = useState("");
+  const [isEditable, setIsEditable] = useState(true);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setUsername(event.target.value);
+    onChange("", false);
+  };
+
+  const handleSubmit = () => {
+    setIsEditable(!isEditable);
+    onChange(username, isEditable);
+  };
+
+  return (
+    <VStack spacing={4}>
+      <InputGroup>
+        <Input
+          value={username}
+          onChange={handleChange}
+          isReadOnly={!isEditable}
+          placeholder="Enter username"
+          backgroundColor={!isEditable ? "gray.200" : "white"}
+          borderColor={!isEditable ? "gray.300" : "gray.300"}
+          _placeholder={{ color: !isEditable ? "gray.500" : "gray.400" }}
+        />
+        <InputRightElement>
+          <IconButton
+            aria-label="Save username"
+            icon={isEditable ? <UnlockIcon /> : <LockIcon />}
+            onClick={handleSubmit}
+          />
+        </InputRightElement>
+      </InputGroup>
+    </VStack>
+  );
+}
